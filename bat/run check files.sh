8a2188eb13713ac67e5c4a9ca817eda8662cfe9a
CORP\nkosinathi.khumalo <nkosinathi.khumalo@irdeto.com>
2019-08-06 14:58:21 +0200

    Deploy - automated deploy

--- bat/run check files.sh
+++ bat/run check files.sh
@@ -1,2 +1,2 @@
 echo  "Run groovy script - Check files"
-"C:\Program Files (x86)\Jenkins\tools\hudson.plugins.groovy.GroovyInstallation\Groovy1.5.4\bin\groovy.bat" "groovyscripts/monitorFiles.groovy"
+"/snap/bin/groovy" "groovyscripts/monitorFiles.groovy"
--- bat/runGenerate.sh
+++ bat/runGenerate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo  "Run groovy script"
-"/var/lib/jenkins/tools/hudson.plugins.groovy.GroovyInstallation/Gbroovy_2.5.7/bin/groovy.bat" "groovyscripts/generate.groovy"
+"/snap/bin/groovy" "groovyscripts/generate.groovy"
